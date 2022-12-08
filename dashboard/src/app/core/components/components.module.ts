e3ba1ae63dfc700207ceb02f4d64da9d62f00e77
carolinergomes <caroline.gomes@estudante.ifms.edu.br>
2022-12-08 00:55:47 -0300

    upd components

--- dashboard/src/app/core/components/components.module.ts
+++ dashboard/src/app/core/components/components.module.ts
@@ -2,7 +2,6 @@ import { MaterialModule } from './../../material/material/material.module';
 import { RouterModule } from '@angular/router';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -19,7 +18,6 @@ import { Imagem5Component } from './imagem5/imagem5.component';
 
 const COMPONENTS = [
   SidebarComponent,
-  HeaderComponent,
   CardGraphComponent,
   CardGraph2Component,
   DashboardComponent,
